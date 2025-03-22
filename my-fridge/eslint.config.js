b5e905f3f16bc9085daef30c9417e36e601ea6a3
gingerbeerlime <lsumin1127@gmail.com>
2025-03-22 19:51:16 +0900

    chore: set up eslint and prettier

--- my-fridge/eslint.config.js
+++ my-fridge/eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -19,10 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error', // prettier의 포맷팅 에러를 lint 에러 리포팅으로 볼 수 있게 하는 규칙
     },
   },
 )
