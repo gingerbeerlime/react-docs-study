2f22c3461484a3a6ce5a8003587ba1f731263f0c
gingerbeerlime <lsumin1127@gmail.com>
2025-05-02 00:20:45 +0900

    chore:set project config

---
+++ managing-state/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+      <div className='flex flex-col items-center justify-center min-h-svh'>
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
