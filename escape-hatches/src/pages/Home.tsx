a3f9697a56bb07f3f97dc3e7a87c6ea401be7bdf
gingerbeerlime <lsumin1127@gmail.com>
2025-05-19 22:02:50 +0900

    chore: initial project setup

---
+++ escape-hatches/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// const buttonClass = 'bg-violet-400 hover:bg-violet-500 cursor-pointer'
+
+const Home = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen px-6 py-12'>
+      <div className='w-full max-w-sm p-5 rounded-lg flex flex-col items-center justify-center gap-4 bg-violet-50 shadow-md'>
+        <h1 className='text-2xl font-bold text-center mb-4'>탈출구</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
